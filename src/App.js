a00f615b5dd6a78c8c66e6a1d8831be432ae9835
Jack Henry <jackson.lewis.henry@gmail.com>
2021-02-17 17:06:29 -0500

    done with mvp

--- src/App.js
+++ src/App.js
@@ -1,73 +1,53 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./App.css";
-// import Details from './Details'
-import { BASE_URL, API_KEY } from './index'
+import Details from './Details'
+// import { BASE_URL, API_KEY } from './index.js'
 
 function App() {
   // const [nasadat, SetNasaDat] = useState([]);
   // const [todaysPic, SetTodaysPic] = useState(null);
 
-  const [nasaData, setNasaData] = useState({})
+  // const [nasaData, setNasaData] = useState([]);
 
-    useEffect(() => {
-        console.log(` fetching nasa data`);
-        axios
-        .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
-        .then((res) => {
-            setNasaData(res.data)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-        return () => {
-            console.log(`Cleanup for nasadata fetch`);
-        };
-    })
-
-
-    // nas
-    return(
-      <div className='App'>
-          <h1>this is the nasa data</h1>
-          {
-              <>
-                  <p><b>Title</b>: {nasaData.title}</p>
-                  <img src={nasaData.hdurl} alt='Photo of The Day'></img>
-                  <p><b>Copright</b>: {nasaData.copyright}</p>
-                  <p><b>Date</b>: {nasaData.date}</p>
-                  <a className='App-link' href={nasaData.url}>Image URL</a>
-                  <p><b>Explanation</b>: {nasaData.explanation}</p>
-              </>
-          }
-      </div>
-  );
+  //   useEffect(() => {
+  //       console.log(` fetching nasa data`);
+  //       axios
+  //       .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+  //       .then((res) => {
+  //         debugger
+  //           setNasaData(res.data)
+  //       })
+  //       .catch((err) => {
+  //           console.log(err);
+  //       })
+  //       return () => {
+  //           console.log(`Cleanup for nasadata fetch`);
+  //       };
+  //   }, [])
 
 
-
-
-
-
-
-
-
-    // return (
-    //   <App />
-    // )
-  // return (
-  //   <div className="App">
-  //     <div className="Header">
-  //       <h1>Hey welcome placeholder stuff</h1>
+  //   return(
+  //     <div className='App'>
+  //         <h1>this is the nasa data</h1>
+  //         {
+  //             <div className='App-header'>
+  //                 <p><b>Title</b>: {nasaData.title}</p>
+  //                 <img src={nasaData.hdurl} alt='Photo of The Day'></img>
+  //                 <p><b>Copright</b>: {nasaData.copyright}</p>
+  //                 <p><b>Date</b>: {nasaData.date}</p>
+  //                 <a className='App-link' href={nasaData.url}>Image URL</a>
+  //                 <p><b>Explanation</b>: {nasaData.explanation}</p>
+  //             </div>
+  //         }
   //     </div>
-  //     <div className="Picdiv">
-  //       <h3>NASA Pic of the day:</h3>
-  //       <img className="Nasapic" href='#'></img>
-  //       <h3>copyright</h3>
-  //       <h3>Set (insert date)</h3>
-  //       <p>explanation</p>
-  //     </div>
-  //   </div>
   // );
+  return (
+    <div>
+    <Details />
+    </div>
+  )
+
 }
 
 /* <p>
--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { BASE_URL, API_KEY } from './index.js'
 
 export default function Details(){
-    const [nasaData, setNasaData] = useState(null)
+    const [nasaData, setNasaData] = useState([]);
 
     useEffect(() => {
         console.log(` fetching nasa data`);
         axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=aRGpKhs417QUpGWeh2fnKSYNPsglpnFn3pQ9wIur`)
+        .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
         .then((res) => {
+        //   debugger
+        //   console.log(res.data)
             setNasaData(res.data)
         })
         .catch((err) => {
@@ -17,29 +20,24 @@ export default function Details(){
         return () => {
             console.log(`Cleanup for nasadata fetch`);
         };
-    })
-
+    }, [])
 
 
     return(
-        <div className='container'>
-            <h1>this is the nasa data</h1>
-            {
-                nasaData &&
-                <>
-                    <p><b>Title</b>: {nasaData.title}</p>
-                    <img src={nasaData.hdurl}></img>
-                    <p><b>Copright</b>: {nasaData.copyright}</p>
-                    <p><b>Date</b>: {nasaData.date}</p>
-                    <a href={nasaData.url}>Image URL</a>
-                    <p><b>Explanation</b>: {nasaData.explanation}</p>
-                
-                
-                </>
-            }
-        </div>
-    );
-
+      <div className='App'>
+          <h1>The Nasa Photo of the Day</h1>
+          {
+              <div className='App-header'>
+                  <p><b>Title</b>: {nasaData.title}</p>
+                  <img src={nasaData.hdurl} alt='Photo of The Day'></img>
+                  <p><b>Copright</b>: {nasaData.copyright}</p>
+                  <p><b>Date</b>: {nasaData.date}</p>
+                  <a className='App-link' href={nasaData.url}>Image URL</a>
+                  <p><b>Explanation</b>: {nasaData.explanation}</p>
+              </div>
+          }
+      </div>
+  );
 
 
 
